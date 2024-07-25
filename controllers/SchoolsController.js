e0e27429e170c532f4019bc38ba52d935ad34d17
Kamil Błoński <kblonski02@gmail.com>
2024-07-25 22:58:38 +0200

    Add SchoolsController

---
+++ controllers/SchoolsController.js
@@ -0,0 +1,37 @@
+import { User, School, Subject, Grade} from '../models/schemas.js';
+
+export class SchoolsController{
+    async getAll(){
+        return await School.findAll({});
+    }
+
+    async createSchool(schoolData, directorDb){
+        const schoolDb = await School.create({
+            ...schoolData
+        });
+
+        if (directorDb) await schoolDb.setDirector(directorDb);
+
+        return schoolDb;
+    }
+
+    async setDirector(schoolDb, directorDb){
+        if (schoolDb && directorDb){
+            await schoolDb.setDirector(directorDb);
+            return true;
+        }
+        return false;
+    }
+
+    async getById(id){
+        await School.findByPk(id);
+    }
+
+    async updateById(id, schoolData){
+        const updatedSchool = await School.update({
+            ...schoolData
+        }, {where: {id: id}});
+        
+        return updatedSchool;
+    }
+}
