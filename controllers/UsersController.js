fb872990540cf149f2f2c04e72e329b1ea272a15
Kamil Błoński <kblonski02@gmail.com>
2024-07-25 22:52:40 +0200

    Add Users Controller

---
+++ controllers/UsersController.js
@@ -0,0 +1,61 @@
+import bcrypt from 'bcryptjs';
+import { User, School, Grade, Subject } from '../models/schemas.js';
+
+export class UsersController {
+    async getAll(){
+        return await User.findAll({});
+    }
+
+    async getAllUsersByRole(role){
+        return await User.findAll({
+            where: {
+                role: role
+            }
+        })
+    }
+
+    async createUser(userData, schoolDb){
+        const salt = await bcrypt.genSalt(10);
+        userData.password = await bcrypt.hash(userData.password, salt);
+        
+        const userDb = await User.create(userData);
+
+        if (schoolDb) await userDb.setSchool(schoolDb);
+
+        return userDb;
+    }
+
+    async validPassword(password, userDb){
+        try {
+            return await bcrypt.compare(password, userDb.password);
+        } catch (err) {
+            throw new Error(err);
+        }
+    }
+
+    async setSchool(userDb, schoolDb){
+        if (userDb && schoolDb){
+            await userDb.setSchool(schoolDb)
+        };
+        return false;
+    }
+
+    async getById(id) {
+        return await User.findByPk(id);
+    }
+
+    async updateById(id, userData){
+        const updatedUser = await User.update({
+            ...userData
+        }, {
+            where: {
+                id: id
+            }
+        }) 
+    }
+
+    async getUserByEmail(email){
+        return await User.findOne({where: { email: email}})
+    }
+    
+};
