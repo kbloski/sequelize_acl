1a594c217d5675aeb2804e0755b26153cb739131
Kamil Błoński <kblonski02@gmail.com>
2024-07-27 18:05:18 +0200

    Add selected funcion to htmlHelper

--- helpers/htmlHelper.js
+++ helpers/htmlHelper.js
@@ -17,6 +17,30 @@ class HtmlHelper {
         return html + '\n</select>';
     };
 
+    // arr = ['admin', 'teacher']
+    getSelectCodeFromArr(arr, selectName, id, className, selectedValue){
+        let idCode  = '';
+        if (id) idCode = `id='${id}'`;
+
+        let html = `<select name='${selectName}' ${idCode} class='${className}'>`;
+
+        for (const str of arr) {
+            let data = '';
+            if (selectedValue === str) data = 'selected';
+
+            html += `\n <option value='${str}' ${data}>${str}</option>`;
+        }
+
+        return html + '\n </select>'
+    };
+
+
+    getElValueFromArrById(arr, id, propertyToShow = 'id'){
+        for ( const el of arr ){
+            if (el.id == id) return el[propertyToShow];
+        };
+        return null;
+    }   
 };
 
 const htmlHelper = new HtmlHelper();
