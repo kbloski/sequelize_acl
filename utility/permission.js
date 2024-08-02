6e6c3ef35ce1bfcbdc9d6967a3ca76d11b637af7
Kamil Błoński <kblonski02@gmail.com>
2024-08-02 14:40:42 +0200

    Upload permissions and priority

--- utility/permission.js
+++ utility/permission.js
@@ -1,24 +1,52 @@
 const usersRoles = [
     {
         role: 'admin',
+        priority: 5,
         allows: [
-            { resource: '/admin/users', permissions: '*' },
-            { resource: '/admin/users/add', permissions: '*' },
-            { resource: '/admin/users/edit/:id', permissions: '*'},
+           
             { resource: '/admin/schools', permissions: '*'},
             { resource: '/admin/schools/add', permissions: '*'},
             { resource: '/admin/schools/edit/:id', permissions: "*" },
-            { resource: '/admin/schools/edit', permissions: '*'}
         ]
     },
     {
-        role: 'user',
+        role: 'director',
+        priority: 4,
+        allows: [
+            { resource: '/admin/schools/myschool', permissions: '*'},
+            { resource: '/admin/schools/view/:id', permissions: '*'},
+            { resource: '/admin/users', permissions: '*' },
+            { resource: '/admin/users/add', permissions: '*' },
+            { resource: '/admin/users/edit/:id', permissions: '*'},
+            { resource: '/admin/users/view/:id', permissions: '*'},
+            { resource: '/subjects/add', permissions: '*'},
+            { resource: '/subjects/edit/:id', permissions: '*'},
+        ]
+    },
+    {
+        role: 'teacher',
+        priority: 3,
+        allows: [
+            { resource: '/subjects', permissions: '*'},
+            { resource: '/subjects/view/:id', permissions: '*'},
+            { resource: '/subjects/view/:subjectId/addstudent', permissions: '*'},
+            { resource: '/subjects/view/:subjectId/student/:studentId/addgrade', permissions: '*'},
+            { resource: '/grades', permissions: '*'},
+            { resource: '/grades/add', permissions: '*'},
+            { resource: '/grades/view/:id', permissions: '*'},
+            { resource: '/grades/edit/:id', permissions: '*'},
+        ]
+    },
+    {
+        role: 'student',
+        priority: 2,
         allows: [
             { resource: '/dashboard', permissions: [ 'post', 'get ']}
         ]
     },
     {
         role: 'guest',
+        priority: 1,
         allows: [
             { resource: '/', permissions: [ 'get' ]}
         ]
@@ -34,7 +62,7 @@ const permissions = {
         targetData.allows = targetData.allows.concat( sourceData.allows );
     },
     isResourceAllowedForUser: function (userRole, resource, method){
-        if (userRole === 'admin') return true;
+        if (userRole === 'admin') return true; // allow for all to admin
         
         const roleData  = this.usersRoles.find( v => v.role == userRole);
 
@@ -54,12 +82,19 @@ const permissions = {
         }
 
         return false;
+    },
+    getPriorityByRole: function(role){
+        const user = this.userRole.find( v => v.role === role);
+        if (user) return user.priority;
+        return -1;
     }
 };
 
-permissions.addRoleParrents('user', 'guest');
-permissions.addRoleParrents('admin', 'user');
-// console.log( JSON.stringify(usersRoles, null, 4))
+permissions.addRoleParrents('student', 'guest');
+permissions.addRoleParrents('teacher', 'student');
+permissions.addRoleParrents('director', 'teacher');
+permissions.addRoleParrents('admin', 'director');
+// console.log(usersRoles)
 
 export {
     permissions
